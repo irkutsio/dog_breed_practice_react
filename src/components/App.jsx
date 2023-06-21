18dd9f627baa9bbcf4beae27ca3fb63ed4b55000
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-06-21 19:02:02 +0300

    added_hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,97 @@
-import { Component } from 'react';
+import { useRef, useState } from 'react';
 import { BreedSelect } from './BreedSelect/BreedSelect';
 import { fetchDogByBreed } from 'api';
 import { Dog } from './Dog/Dog';
 import { ImageLoader } from './DogSkeleton/DogSkeleton';
 
-export class App extends Component {
-  abortCtrl;
+export const App = () => {
+  const [dog, setDog] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  state = {
-    dog: null,
-    isLoading: false,
-    error: null,
-  };
+  const abortCtrl = useRef()
 
-  fetchDog = async breedId => {
-    if (this.abortCtrl) {
-      this.abortCtrl.abort();
-    }
-    this.abortCtrl = new AbortController();
+  const fetchDog = async breedId => {
+    if (abortCtrl.current) {
+            abortCtrl.current.abort();
+          }
+          abortCtrl.current = new AbortController();
     try {
-      this.setState({ isLoading: true, error: null });
+      setIsLoading(true);
+      setError(null);
       const fetchedDog = await fetchDogByBreed({
         breedId,
-        abortCtrl: this.abortCtrl,
+        abortCtrl: abortCtrl.current,
       });
-      this.setState({ dog: fetchedDog });
+      setDog(fetchedDog);
     } catch (error) {
       if (error.code !== 'ERR_CANCELED') {
-        this.setState({ error: 'oops...try again' });
+        setError('oops...try again');
       }
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  render() {
-    const { dog, isLoading, error } = this.state;
-    return (
-      <div
-        style={{
-          padding: '50px',
-        }}
-      >
-        <BreedSelect onSelect={this.fetchDog} />
+  return (
+    <div
+      style={{
+        padding: '50px',
+      }}
+    >
+      <BreedSelect onSelect={fetchDog} />
 
-        {isLoading && <ImageLoader/>}
-        {dog && !isLoading && <Dog dog={dog} />}
-        {error && <p>{error}</p>}
-      </div>
-    );
-  }
-}
+      {isLoading && <ImageLoader />}
+      {dog && !isLoading && <Dog dog={dog} />}
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   abortCtrl;
+
+//   state = {
+//     dog: null,
+//     isLoading: false,
+//     error: null,
+//   };
+
+//   fetchDog = async breedId => {
+//     if (this.abortCtrl) {
+//       this.abortCtrl.abort();
+//     }
+//     this.abortCtrl = new AbortController();
+//     try {
+//       this.setState({ isLoading: true, error: null });
+//       const fetchedDog = await fetchDogByBreed({
+//         breedId,
+//         abortCtrl: this.abortCtrl,
+//       });
+//       this.setState({ dog: fetchedDog });
+//     } catch (error) {
+//       if (error.code !== 'ERR_CANCELED') {
+//         this.setState({ error: 'oops...try again' });
+//       }
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   render() {
+//     const { dog, isLoading, error } = this.state;
+//     return (
+//       <div
+//         style={{
+//           padding: '50px',
+//         }}
+//       >
+//         <BreedSelect onSelect={this.fetchDog} />
+
+//         {isLoading && <ImageLoader/>}
+//         {dog && !isLoading && <Dog dog={dog} />}
+//         {error && <p>{error}</p>}
+//       </div>
+//     );
+//   }
+// }
--- src/components/BreedSelect/BreedSelect.jsx
+++ src/components/BreedSelect/BreedSelect.jsx
@@ -1,59 +1,111 @@
 import { fetchBreeds } from 'api';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
 
+export const BreedSelect = ({ onSelect }) => {
+  const [breeds, setBreeds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const options = breeds.map(breed => {
+    return {
+      value: breed.id,
+      label: breed.name,
+    };
+  });
 
-export class BreedSelect extends Component {
-  abortCtrl;
+  const abortCtrl = new AbortController();
 
-  state = {
-    breeds: [],
-    isloading: false,
-    error: null,
-  };
-  async componentDidMount() {
-    try {
-      this.abortCtrl = new AbortController();
-      this.setState({ isloading: true, error: null });
-      const fetchedBreeds = await fetchBreeds(this.abortCtrl);
-      this.setState({ breeds: fetchedBreeds });
-    } catch (error) {
-      if (error.code !== 'ERR_CANCELED') {
-        this.setState({
-          error: 'OOoops.....something went wrong 😖 try to reload the page',
-        });
+  useEffect(() => {
+    async function getBreeds() {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const fetchedBreeds = await fetchBreeds(abortCtrl.signal);
+
+        setBreeds(fetchedBreeds);
+      } catch (error) {
+        if (error.code !== 'ERR_CANCELED') {
+          setError('Ooops.....something went wrong 😖 try to reload the page');
+        }
+      } finally {
+        setIsLoading(false);
       }
-    } finally {
-      this.setState({ isloading: false });
     }
-  }
+    getBreeds();
 
-  componentWillUnmount() {
-    this.abortCtrl.abort();
-  }
+    return () => {
+      //то же что и componentWillUnmount()
+      abortCtrl.abort();
+    };
+  }, []);
 
-  render() {
-    const {onSelect} = this.props
-    const { breeds, isloading, error } = this.state;
-    const options = breeds.map(breed => {
-      return {
-        value: breed.id,
-        label: breed.name,
-      };
-    });
-    return (
-      <div>
-        <Select
-          isLoading={isloading}
-          options={options}
-          onChange={option => {
-            onSelect(option.value)
-          }}
-        />
-        {error && <p>{error}</p>}
-        {/* {isloading && <p>Is loading.....😊</p>} */}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Select
+        isLoading={isLoading}
+        options={options}
+        onChange={option => {
+          onSelect(option.value);
+        }}
+      />
+      {error && <p>{error}</p>}
+      {/* {isloading && <p>Is loading.....😊</p>} */}
+    </div>
+  );
+};
+
+// export class BreedSelect extends Component {
+//   abortCtrl;
+
+//   state = {
+//     breeds: [],
+//     isloading: false,
+//     error: null,
+//   };
+//   async componentDidMount() {
+//     try {
+//       this.abortCtrl = new AbortController();
+//       this.setState({ isloading: true, error: null });
+//       const fetchedBreeds = await fetchBreeds(this.abortCtrl);
+//       this.setState({ breeds: fetchedBreeds });
+//     } catch (error) {
+//       if (error.code !== 'ERR_CANCELED') {
+//         this.setState({
+//           error: 'OOoops.....something went wrong 😖 try to reload the page',
+//         });
+//       }
+//     } finally {
+//       this.setState({ isloading: false });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.abortCtrl.abort();
+//   }
+
+//   render() {
+//     const {onSelect} = this.props
+//     const { breeds, isloading, error } = this.state;
+//     const options = breeds.map(breed => {
+//       return {
+//         value: breed.id,
+//         label: breed.name,
+//       };
+//     });
+//     return (
+//       <div>
+//         <Select
+//           isLoading={isloading}
+//           options={options}
+//           onChange={option => {
+//             onSelect(option.value)
+//           }}
+//         />
+//         {error && <p>{error}</p>}
+//         {/* {isloading && <p>Is loading.....😊</p>} */}
+//       </div>
+//     );
+//   }
+// }
